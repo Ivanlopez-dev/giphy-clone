d508c95c4e8a128e3309af58829d63436defcd38
Ivan López Rodríguez <ivaan100@gmail.com>
2024-09-10 23:20:36 +0200

    Add: React wouter
    Better performance with wouter, making SPA.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
 import ListOfGifs from './components/ListOfGifs'
 
-export default function App() {
-  const [keyword, setKeyword] = useState('panda')
+import { Link, Route } from 'wouter'
 
+export default function App() {
   return (
     <div className='App'>
       <section className='App-content'>
-        <button onClick={() => setKeyword('mapache')}>Cambiar keyword</button>
-        <ListOfGifs keyword={keyword} />
+        <h1>App</h1>
+        <Link to='/gif/simpsons'>Gifs de Los Simpsons</Link>
+        <Link to='/gif/futurama'>Gifs de Futurama</Link>
+        <Link to='/gif/rick'>Gifs de Rick y Morty</Link>
+        <Route
+          component={ListOfGifs}
+          path='/gif/:keyword'
+        />
       </section>
     </div>
   )
