9d7c6354ac50a75b9b45fafaedc3281d19a8c1d5
Ivan López Rodríguez <ivaan100@gmail.com>
2024-09-12 14:03:08 +0200

    Add: global state using context
    And add the detail recovered from global state

--- src/App.js
+++ src/App.js
@@ -3,29 +3,40 @@ import './App.css'
 import Home from './pages/Home'
 import SearchResults from './pages/SearchResults'
 import Detail from './pages/Detail'
+import StaticContext from './context/StaticContext'
+import { GifsContextProvider } from './context/GifsContext'
 
 import { Link, Route } from 'wouter'
 
 export default function App() {
   return (
-    <div className='App'>
-      <section className='App-content'>
-        <Link to='/'>
-          <p>GIFFY CLONE</p>
-        </Link>
-        <Route
-          component={Home}
-          path='/'
-        />
-        <Route
-          component={SearchResults}
-          path='/search/:keyword'
-        />
-        <Route
-          component={Detail}
-          path='/gif/:id'
-        />
-      </section>
-    </div>
+    <StaticContext.Provider
+      value={{
+        name: 'lorodevs',
+        suscribeteAlCanal: true,
+      }}
+    >
+      <div className='App'>
+        <section className='App-content'>
+          <Link to='/'>
+            <p>GIFFY</p>
+          </Link>
+          <GifsContextProvider>
+            <Route
+              component={Home}
+              path='/'
+            />
+            <Route
+              component={SearchResults}
+              path='/search/:keyword'
+            />
+            <Route
+              component={Detail}
+              path='/gif/:id'
+            />
+          </GifsContextProvider>
+        </section>
+      </div>
+    </StaticContext.Provider>
   )
 }
--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -2,10 +2,13 @@ import React from 'react'
 import { Link } from 'wouter'
 import './Gif.css'
 
-export default function Gif({ title, url }) {
+export default function Gif({ title, url, id }) {
   return (
     <div className='Gif'>
-      <Link>
+      <Link
+        to={`/gif/${id}`}
+        className='Gif-link'
+      >
         <h4>{title}</h4>
         <img
           alt={title}
