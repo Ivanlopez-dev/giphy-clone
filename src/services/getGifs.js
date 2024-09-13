295b3264403405ab0ea54ef64a0b5eeb3c9db891
Ivan López Rodríguez <ivaan100@gmail.com>
2024-09-13 12:29:23 +0200

    Add: new getTrending service

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,19 +1,22 @@
-const apiKey = 'NhSj8FZfoeireWVpFSleQUDZ01fGKkvN'
+import { API_KEY, API_URL } from './settings'
 
-export default function getGifs({ keyword = 'futurama' } = {}) {
-  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+const fromApiResponseToGifs = apiResponse => {
+  const { data = [] } = apiResponse
+  if (Array.isArray(data)) {
+    const gifs = data.map(image => {
+      const { images, title, id } = image
+      const { url } = images.fixed_height_downsampled
+      return { title, id, url }
+    })
+    return gifs
+  }
+  return []
+}
+
+export default function getGifs({ limit = 25, keyword = 'futurama' } = {}) {
+  const apiURL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
 
   return fetch(apiURL)
     .then(res => res.json())
-    .then(response => {
-      const { data } = response
-      if (Array.isArray(data)) {
-        const gifs = data.map(image => {
-          const { images, title, id } = image
-          const { url } = images.fixed_height_downsampled
-          return { title, id, url }
-        })
-        return gifs
-      }
-    })
+    .then(fromApiResponseToGifs)
 }
