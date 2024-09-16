ecc1879232470559ccf65bec12738a12d3184975
Ivan López Rodríguez <ivaan100@gmail.com>
2024-09-16 11:42:19 +0200

    Add: lazy load on Trending component
    Using IntersectionObserver and isIntersecting param.
    Also add min-height on ListOfGifs css, forcing push down Trending component render.

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Category from 'components/Category'
 import getTrendingTerms from 'services/getTrendingTermsService'
 
-export default function TrendingSearches() {
+function TrendingSearches() {
   const [trends, setTrends] = useState([])
 
   useEffect(function () {
@@ -15,4 +15,25 @@ export default function TrendingSearches() {
       options={trends}
     />
   )
+}
+
+export default function LazyTrending() {
+  const [show, setShow] = useState(false)
+
+  useEffect(function () {
+    const onChange = entries => {
+      const el = entries[0]
+      if (el.isIntersecting) {
+        setShow(true)
+      }
+    }
+
+    const observer = new IntersectionObserver(onChange, {
+      rootMargin: '100px',
+    })
+
+    observer.observe(document.getElementById('LazyTrending'))
+  })
+
+  return <div id='LazyTrending'>{show ? <TrendingSearches /> : null}</div>
 }
