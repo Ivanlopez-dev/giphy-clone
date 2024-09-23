f759983b01d2e1abae711785ef2394b0a553ad1e
Ivan López Rodríguez <ivaan100@gmail.com>
2024-09-23 18:53:56 +0200

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -22,7 +22,7 @@ export default function Home() {
       <SearchForm onSubmit={handleSubmit} />
       <div className='App-main'>
         <div className='App-results'>
-          <h3 className='App-title'>Latest search</h3>
+          <h3 className='App-title'>Latest search ...</h3>
           <ListOfGifs gifs={gifs} />
         </div>
         <div className='App-category'>
