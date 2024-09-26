ddf55dec8000162d805910dc3c53a9dff8b5842d
Ivan López Rodríguez <ivaan100@gmail.com>
2024-09-26 13:01:47 +0200

    Add: useReducer custom hook
    This commit extracts useReducer into a custom hook. This way SearchForm component its cleaner and easier to read and more escalable.

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,48 +1,24 @@
-import React, { useReducer } from 'react'
+import React from 'react'
 import { useLocation } from 'wouter'
 import css from './SearchForm.module.css'
+import useForm from './hook'
 import Button from 'components/Button'
 
 const RATINGS = ['g', 'pg', 'pg-13', 'r']
-
-const ACTIONS = {
-  UPDATE_KEYWORD: 'update_keyword',
-  UPDATE_RATING: 'update_rating',
-}
-
-const ACTIONS_REDUCERS = {
-  [ACTIONS.UPDATE_KEYWORD]: (state, action) => ({
-    ...state,
-    keyword: action.payload,
-    times: state.times + 1,
-  }),
-  [ACTIONS.UPDATE_RATING]: (state, action) => ({
-    ...state,
-    rating: action.payload,
-  }),
-}
-
-const reducer = (state, action) => {
-  const actionReducer = ACTIONS_REDUCERS[action.type]
-  return actionReducer ? actionReducer(state, action) : state
-}
 
 export default function SearchForm({
   initialKeyword = '',
   initialRating = 'g',
 }) {
-  const [state, dispatch] = useReducer(reducer, {
-    keyword: decodeURIComponent(initialKeyword),
-    rating: initialRating,
-    times: 0,
+  const { keyword, rating, times, updateKeyword, updateRating } = useForm({
+    initialKeyword,
+    initialRating,
   })
-
-  const { keyword, rating, times } = state
 
   const [_, pushLocation] = useLocation()
 
   const handleChange = evt => {
-    dispatch({ type: ACTIONS.UPDATE_KEYWORD, payload: evt.target.value })
+    updateKeyword(evt.target.value)
   }
 
   const handleSubmit = evt => {
@@ -52,7 +28,7 @@ export default function SearchForm({
   }
 
   const handleChangeRating = evt => {
-    dispatch({ type: ACTIONS.UPDATE_RATING, payload: evt.target.value })
+    updateRating(evt.target.value)
   }
 
   return (
