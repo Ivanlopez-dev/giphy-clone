e78b32ed8589ae46cea428dcb6ea9221f31c0543
Ivan López Rodríguez <ivaan100@gmail.com>
2024-10-01 15:58:30 +0200

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -19,7 +19,7 @@ export default function Home() {
       <div className='App-wrapper'>
         <div className='App-main'>
           <div className='App-results'>
-            <h3 className='App-title'>Latest search</h3>
+            <h3 className='App-title'>Last search</h3>
             <ListOfGifs gifs={gifs} />
           </div>
           <div className='App-category'>
